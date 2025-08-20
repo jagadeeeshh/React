bf96c77c3d08f67e0ad25c7b93a05580e8261091
jagadeeeshh <jagu.kannan@gmail.com>
2025-08-21 00:39:28 +0530

    react setup

---
+++ MyApp/dist/MyApp.17d54b32.js
@@ -0,0 +1,11 @@
+// To create element in react -->contains "id","attributes","children"
+// react --> it contains core files of react
+const heading = React.createElement("h1", {
+    id: "root"
+}, "helloworld");
+// reactDOM --> which is used to modify the DOM or DOM operations
+// 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=MyApp.17d54b32.js.map
