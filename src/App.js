c1f2ea344b8a16eb59a42eaa04caba29440f0978
jagadeeeshh <jagu.kannan@gmail.com>
2025-08-25 11:02:47 +0530

    react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
+//compoment composition- component inside component
+const Title=()=> (<h1>Hello from title</h1>);
+
+const number = 625;
+//Functional components
+const Heading =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Title/>
+   {325+300}
+   {number}
+   {console.log("hi!")}
+   <h1>Welcome to react</h1>
+   <h2>Welcome to react {number}</h2>
+   <h3>Welcome to react</h3>
+   {Title()}
+   </>
   );
 }
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>);
 export default App;
