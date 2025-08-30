709e39252c06d307222c243e8eb3ff6cb8c91bb1
jagadeeeshh <jagu.kannan@gmail.com>
2025-08-30 22:26:57 +0530

    import/export

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
 import App from "./App";
 
 //compoment composition- component inside component
-const Title=()=> (<h1>Hello from title</h1>);
+//const Title=()=> (<h1>Hello from title</h1>);
 
-const number = 625;
+//const number = 625;
 //Functional components
-const Heading =()=> {
-  return (
-   <>
-   <Title/>
-   {325+300}
-   {number}
-   {console.log("hi!")}
-   <h1>Welcome to react</h1>
-   <h2>Welcome to react {number}</h2>
-   <h3>Welcome to react</h3>
-   {Title()}
-   </>
+//const Heading =()=> {
+  //return (
+   //<>
+   //<Title/>
+   //{325+300}
+   //{number}
+   //{console.log("hi!")}
+   //<h1>Welcome to react</h1>
+   //<h2>Welcome to react {number}</h2>
+   //<h3>Welcome to react</h3>
+   //{Title()}
+   //</>
+  //);
+//}
+
+const AppLayout = () =>{
+  return(
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
   );
 }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>);
+root.render(<AppLayout/>);
 export default App;
