00aed39f47b76eb981f993d3b3c72a230bfd2db4
jagadeeeshh <jagu.kannan@gmail.com>
2025-09-14 08:14:47 +0530

    spread oprator,usestatus

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,10 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
+import { Age } from "./components/Age/Age.tsx";
+import { Employee } from "./components/Employee/Employee.tsx";
 
-import { Employee } from "./components/Employee/Employee";
-//import App from "./App";
 
-//compoment composition- component inside component
-//const Title=()=> (<h1>Hello from title</h1>);
-
-//const number = 625;
-//Functional components
-//const Heading =()=> {
-  //return (
-   //<>
-   //<Title/>
-   //{325+300}
-   //{number}
-   //{console.log("hi!")}
-   //<h1>Welcome to react</h1>
-   //<h2>Welcome to react {number}</h2>
-   //<h3>Welcome to react</h3>
-   //{Title()}
-   //</>
-  //);
-//}
 
 const value1 = "Hello";
 const value2 = "React";
@@ -39,13 +20,7 @@ export const AppLayout = () =>{
     country: "USA"
   }
 
-  // const arr1=[1,2,3,4,5,6,7,8];
-  // const arr2 = [9,10,11,12];
-  // const mergarr = [arr1,arr2];
-  // const mergarr1 = [...arr1,...arr2];
 
-  // console.log(mergarr);
-  // console.log(mergarr1);
 
   const obj1 ={
     name : "jaggu",
@@ -75,7 +50,7 @@ export const AppLayout = () =>{
     
     <Employee name="jagadeesh" age = {23} />
     <Employee {...emp1}/>
-
+    <Age/>
     
     </>
   );
--- src/components/Age/Age.tsx
+++ src/components/Age/Age.tsx
@@ -1,6 +1,9 @@
 // import { useState } from "react";
 // const Age = ()=>{
 
+import { useState } from "react";
+import './Age.css';
+
 //     const [count, setcount]=useState(0);
 
 //     return(
@@ -23,3 +26,77 @@
 //     );
 // }
 // export default Age;
+
+export const Age=()=>{
+    //state variable 
+    //const [statevariable, statevariablechange]=useState(initial value)
+    // setcount(count+1) this is not recommended 
+    // when you call setstate like setcount, react doesn't update the state immedietly. Instead , it schedule it, so if you rely on the current value, you might get 
+    const[count1, setCount1]= useState<number>(0);
+    const[count2,setCount2]=useState(0);
+
+    const[todo,setTodo]=useState(["jagadeesh"," BK"]);
+
+    const[todo2,setTodo2]=useState({
+        name: ["jagadeesh"],
+        age: 23,
+    });
+
+    const incrementalCount1=()=>{
+        setCount1(count1+1);
+        setCount1(count1+1);
+        setCount1(count1+1);
+    }
+    
+     const incrementalCount2=()=>{
+        setCount2(prev=>prev+1);
+        setCount2(prev=>prev+1);
+        setCount2(prev=>prev+1);
+    }
+
+    const addTodo=()=>{
+        setTodo([...todo,"jaggu"]);
+    }
+
+     const addTodo2=()=>{
+        setTodo2(prev=>
+        ({
+            ...todo2,
+            name:[prev.name+"jaggu"],
+            age: 24,
+        }))
+    }
+
+
+    return(
+        <>
+            
+            <h1>Count : {count1} </h1>
+            <button onClick={incrementalCount1}>
+              click me to increse the count
+            </button>
+
+            <hr/>
+
+             <h1>Count : {count2} </h1>
+            <button onClick={incrementalCount2}>
+              click me to increse the count
+            </button>
+            
+            <hr/>
+            <h1>Namne : {todo}</h1>
+            <button onClick={addTodo}>
+              click me to add
+            </button>
+
+            <hr/>
+            <h1>Name : {todo2.name} - {todo2.age}</h1>
+            <button onClick={addTodo2}>
+              click me to change
+            </button>
+
+        </>);
+
+        
+
+}
